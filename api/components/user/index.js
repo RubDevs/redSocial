f4df6d87b62c939b8345ee4b2007fb1c49785219
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-21 22:53:25 -0600

    remote mysql db used by API to fetch data, list function ok

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,6 @@
 const controller = require('./controller')
-const store = require('../../../store/mysql')
+//const store = require('../../../store/mysql')
+const store = require('../../../store/remote-mysql')
 
 
 //dependency injection
