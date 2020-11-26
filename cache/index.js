6f90b87b5cd7b842159f7bb4e561892c573f096f
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-26 10:40:37 -0600

    cache microservice using redis ok

---
+++ cache/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const config = require('../config')
+const router = require('./network')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+//routes
+
+app.use('/',router)
+
+app.listen(config.cacheService.port, ()=>{
+    console.log('Servicio de cache escuchando en el puerto ', config.cacheService.port)
+})
